9730bf346a9b0586228fa280f047a60711dfaa02
Marah <marahelhariry@gmail.com>
2024-10-15 15:54:15 +0100

    Add files via upload

---
+++ Statisitical_Analysis/Question 1.R
@@ -0,0 +1,53 @@
+#read dataset in 
+blood <- read.csv('blood_pressure.csv', header = TRUE)
+head(blood)
+summary(blood)
+
+#factoring character variables
+blood$Sex<-factor(blood$Sex)
+blood$Region<-factor(blood$Region)
+
+#Question 1.1#
+#subset dataset to be male only
+male_blood <- subset(blood, Sex == 'Men')
+#identify country with highest prevalence
+highest_country <- male_blood$Country[which.max(male_blood$Prevalence)]
+#identify country with lowest prevalence
+lowest_country <- male_blood$Country[which.min(male_blood$Prevalence)]
+#print results
+cat('highest: ', highest_country, 'lowest:', lowest_country)
+
+#Question 1.2#
+#plot association between Sex, Region and Prevalence 
+library(ggplot2)
+#boxplot
+ggplot(blood, aes(x = Region, y = Prevalence, fill = Sex)) +
+  geom_boxplot(color = "black", outlier.color = "red", width = 0.5) +
+  scale_fill_manual(values = c("#66c2a5", "#fc8d62")) +  
+  labs(x = "Global Region", y = "Prevalence (Proportion)") +
+  theme_gray() +
+  theme(text = element_text(size = 12))
+
+#Question 1.3#
+#model selection
+#check prevalence for a normal distribution
+hist(blood$Prevalence)
+#data shows a normal distribution, so will test for association significance using lm
+
+#maximal model
+blood.model <- lm(Prevalence ~ Sex + Region, data = blood)
+summary(blood.model)
+blood.model2 <- lm(Prevalence ~ Sex * Region, data = blood)
+summary(blood.model2)
+drop1(blood.model, test ='F')
+#visualise
+library(lattice)
+xyplot(Prevalence ~ Region | Sex, data = blood)
+
+anova(blood.model, blood.model2, test ='F')
+#model 2 has a better fit.
+#drop1 test
+anova(blood.model2, test ='F')
+
+
+#both sex and region show a significant association with prevalence
---
+++ Statisitical_Analysis/Question 2.R
@@ -0,0 +1,49 @@
+#install packages
+install.packages("dplyr")
+install.packages("survminer")
+library(dplyr)
+library(survival)
+library(survminer)
+
+#Read data in 
+heart <- read.csv('heart_survival.csv', header = TRUE)
+summary(heart)
+head(heart)
+
+#question 2.1#
+#fit survival curves separately by sex & produce a p value using surv_pvalue.
+sfit <- survfit(Surv(time, event)~sex, data=heart)
+sfit
+summary(sfit)
+surv_pvalue(sfit)
+
+#look at the range of time
+range(heart$time)
+#1 - 2358 days
+
+#create a sequence of numbers going from one number to another number by increments of yet another number
+seq(0, 2358, 100)
+summary(sfit, time=seq(0, 2358, 100))
+
+#we have fit survival curve, we can now visualize it with a Kaplan-Meier plot
+plot(sfit)
+ggsurvplot(sfit, data = heart,
+           ggtheme = theme_gray(),
+           xlab = 'Time (days)', ylab='Survival Probability')
+
+#females have a greater survival probability but men survive longer
+
+#question 2.2#
+#p value
+surv_pvalue(sfit)
+
+#question 2.3#
+time_points <- c(500, 1000, 1500, 2000)
+newdata <- data.frame(sex = c("Male", "Female"))
+#summary to get probabilities at specific timepoints
+surv_summary <- summary(sfit, times = time_points, newdata = newdata)
+
+#survival probabilities
+surv_prob <- surv_summary$surv
+summary(surv_prob)
+print(surv_prob)
